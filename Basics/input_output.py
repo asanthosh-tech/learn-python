2681fcca77b758aca58925b3caa383bdea582942
Santhosh Aavula <asanthosh08.tech@gmail.com>
2026-01-02 06:01:29 +0530

    Python basics: input and output

--- Basics/input_output.py
+++ Basics/input_output.py
@@ -1,19 +1,19 @@
 #Topic-2 : Input and output
 
 #Basic input
-name=input("enter your name:") 
-city=input("enter your city name:") 
+name=input("Enter your name:") 
+city=input("Enter your city name:") 
 print(f"Hello {name} from {city}")
 
 #Two numbers
-a=int(input("enter a value")) 
-b=int(input("enter b value")) 
+a=int(input("Enter 1st number")) 
+b=int(input("Enter 2nd number")) 
 print(f"addition:{a+b}") 
 print(f"difference:{a-b}")
-print(f"product:{a*b}) 
+print(f"product:{a*b}") 
 
 #Multiple inputs
-a,b,c=map(int,range("enter 3 values:"). split()) #splits into 3 separate values
+a,b,c=map(int,input("Enter 3 values:").split()) #splits into 3 separate values
 print(f"sum:{a+b+c}") 
 print(f"average:{(a+b+c)/3}") 
 
@@ -21,3 +21,9 @@ print(f"average:{(a+b+c)/3}")
 birth_year=int(input("enter your birth year")) 
 age=2025-birth_year
 print(f"you are {age} years old") 
+
+#Percentage
+total=int(input("Enter total marks:")) 
+obtained=int(input("Enter obtained marks:")) 
+percentage=(obtained/total) *100
+print(f"percentage :{percentage}") 
